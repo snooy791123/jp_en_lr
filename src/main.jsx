f5d5cc12e73b9479f5619749f77b2e832749d945
snooy791123 <139746068+snooy791123@users.noreply.github.com>
2026-04-27 18:44:59 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// 如果你有獨立的 CSS 檔案 (例如 Tailwind 設定)，記得在這裡引入，例如: import './index.css'
+import './index.css' // 新增這行來載入 Tailwind 樣式！
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
